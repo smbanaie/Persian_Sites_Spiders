e898d132a0a76538ea25ecf40d4ce4c65174fedb
mojta <mojtaba.banaie@gmail.com>
2019-03-14 23:01:54 +0330

    improve the writing to vsc file

--- jobinja_crawler/jobinja_crawler/spiders/jobinja.py
+++ jobinja_crawler/jobinja_crawler/spiders/jobinja.py
@@ -1,66 +1,70 @@
 # -*- coding: utf-8 -*-
 from scrapy.selector import Selector
-from jobinja_crawler.items import JobinjaCompanyItem,JobinjaJobItem
-from scrapy.spiders import CrawlSpider,Rule
+from jobinja_crawler.items import JobinjaCompanyItem, JobinjaJobItem
+from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy import Request
 import codecs
 import logging
 
+
 class JobinjaSpider(CrawlSpider):
     name = "jobinja"
-    allowed_domains = ["www.jobinja.ir","jobinja.ir"]
+    allowed_domains = ["www.jobinja.ir", "jobinja.ir"]
     start_urls = [
-    "https://jobinja.ir/companies"
+        "https://jobinja.ir/companies"
     ]
+
     #
+    def __init__(self):
+        super().__init__()
+        self.write_company_info({}, header=True)
+        self.write_job_info({}, header=True)
+
     rules = [Rule(LinkExtractor(allow=r'/companies/[^/]+$', ), callback='parse_company', follow=False),
-             Rule(LinkExtractor(allow=r'/companies/[a-zA-Z0-9\-_]+/jobs$', ), callback='parse_company_jobs', follow=True),
+             Rule(LinkExtractor(allow=r'/companies/[a-zA-Z0-9\-_]+/jobs$', ), callback='parse_company_jobs',
+                  follow=True),
              Rule(LinkExtractor(allow=r'/companies/[a-zA-Z0-9\-_]+/jobs/\.*', ), callback='parse_jobs', follow=False),
              Rule(LinkExtractor(allow=[r'\/companies\?page=\d+', r'\/companies$'], ), follow=True)
              ]
     # logger = logging.getLogger(__name__)
 
-    company_details  = r"companies_info.csv"
+    company_details = r"companies_info.csv"
     company_jobs_details = r"companies_jobs.csv"
 
     def parse_company(self, response):
-        yield Request(response.request.url +"/jobs",
-                      meta = {
+        yield Request(response.request.url + "/jobs",
+                      meta={
                           'dont_redirect': True,
                           'handle_httpstatus_list': [302]
-              }, callback=self.parse_company_jobs)
+                      }, callback=self.parse_company_jobs)
 
     def parse_company_jobs(self, response):
         item = JobinjaCompanyItem()
         tiltle = response.xpath('//h2[@class="c-companyHeader__name"]/text()').extract()
         item["title_fa"] = tiltle[0]
         item["title_en"] = tiltle[1]
-        item["open_jobs"] = response.css('.c-companyHeader__navigatorOpenJobs::text').  extract_first()
+        item["open_jobs"] = response.css('.c-companyHeader__navigatorOpenJobs::text').extract_first()
         company_metadata = [x.strip() for x in
-                    response.xpath('//div[@class="c-companyHeader__meta"]/span//text()').extract() if x.strip() != ""]
+                            response.xpath('//div[@class="c-companyHeader__meta"]/span//text()').extract() if
+                            x.strip() != ""]
 
         if len(company_metadata) == 4:
             item["year"] = company_metadata[0].strip()
-            item["category"]  = company_metadata[1].strip()
-            item["company_size"]  = company_metadata[2].strip()
-            item["company_site"]  = company_metadata[3].strip()
+            item["category"] = company_metadata[1].strip()
+            item["company_size"] = company_metadata[2].strip()
+            item["company_site"] = company_metadata[3].strip()
         elif len(company_metadata) == 3:
             item["category"] = company_metadata[0].strip()
             item["company_size"] = company_metadata[1].strip()
             item["company_site"] = company_metadata[2].strip()
             item["year"] = str(None)
-        else :
+        else:
             item["category"] = company_metadata[0].strip()
             item["company_size"] = company_metadata[1].strip()
             item["year"] = str(None)
             item["company_site"] = str(None)
-        f = codecs.open(self.company_details, "a", encoding="utf-8")
-        row_csv = "{0},{1},{2},{3},{4},{5},{6}\r\n".format(item["title_fa"], item["title_en"], item["open_jobs"],
-                                                           item["category"], item["company_size"], item["company_site"],
-                                                           item["year"])
-        f.write(row_csv)
-        f.close()
+        self.write_company_info(item)
         yield item
 
     def parse_jobs(self, response):
@@ -70,27 +74,28 @@ class JobinjaSpider(CrawlSpider):
 
         item["active"] = True
         submit_cv = response.xpath("//input[@value='ارسال رزومه']").get()
-        if submit_cv == None :
+        if submit_cv == None:
             item["active"] = False
 
-        categories = response.css(".c-jobView__firstInfoBox")\
+        categories = response.css(".c-jobView__firstInfoBox") \
             .xpath(".//li[h4/text()='دسته‌بندی شغلی']//span/text()").getall()
-        item["category"] =""
+        item["category"] = ""
         if categories:
             item["category"] = "^".join([cat.strip() for cat in categories if cat != ""])
-        location = response.css(".c-jobView__firstInfoBox")\
+        location = response.css(".c-jobView__firstInfoBox") \
             .xpath(".//li[h4/text()='موقعیت مکانی']//span/text()").getall()
         item["location"] = ""
         if location:
-            item["location"] =  "^".join([ " ".join([word.strip() for word in  loc.split() if word!=""]) for loc in location if loc != ""])
-        jobType = response.css(".c-jobView__firstInfoBox")\
+            item["location"] = "^".join(
+                [" ".join([word.strip() for word in loc.split() if word != ""]) for loc in location if loc != ""])
+        jobType = response.css(".c-jobView__firstInfoBox") \
             .xpath(".//li[h4/text()='نوع همکاری']//span/text()").getall()
-        item["jobType"]=""
+        item["jobType"] = ""
         if jobType:
             item["jobType"] = "^".join([jt.strip() for jt in jobType if jt != ""])
-        minExperience = response.css(".c-jobView__firstInfoBox")\
+        minExperience = response.css(".c-jobView__firstInfoBox") \
             .xpath(".//li[h4/text()='حداقل سابقه کار']//span/text()").getall()
-        item["minExperience"]=""
+        item["minExperience"] = ""
         if minExperience:
             item["minExperience"] = "^".join([me.strip() for me in minExperience if me != ""])
 
@@ -98,9 +103,10 @@ class JobinjaSpider(CrawlSpider):
             .xpath(".//li[h4/text()='حقوق']//span/text()").getall()
         item["salary"] = ""
         if salary:
-            item["salary"] = "^".join([ " ".join([word.strip() for word in  me.split() if word!=""]) for me in salary if me != ""])
+            item["salary"] = "^".join(
+                [" ".join([word.strip() for word in me.split() if word != ""]) for me in salary if me != ""])
 
-        period = response.css(".c-jobView__firstInfoBox")\
+        period = response.css(".c-jobView__firstInfoBox") \
             .xpath(".//li[h4/text()='تاریخ شروع و پایان همکاری']//span/text()").getall()
         item["period"] = ""
         if period:
@@ -119,11 +125,13 @@ class JobinjaSpider(CrawlSpider):
             item["gender"] = "^".join(
                 [" ".join([word.strip() for word in gn.split() if word != ""]) for gn in gender if gn != ""])
 
-        allowedMajors = response.xpath("//ul[@class='c-infoBox']/li[h4/text()='رشته‌های تحصیلی مرتبط']/div/span/text()").getall()
+        allowedMajors = response.xpath(
+            "//ul[@class='c-infoBox']/li[h4/text()='رشته‌های تحصیلی مرتبط']/div/span/text()").getall()
         item["allowedMajors"] = ""
         if allowedMajors:
             item["allowedMajors"] = "^".join(
-                [" ".join([word.strip() for word in ajob.split() if word != ""]) for ajob in allowedMajors if ajob != ""])
+                [" ".join([word.strip() for word in ajob.split() if word != ""]) for ajob in allowedMajors if
+                 ajob != ""])
 
         degree = response.xpath(
             "//ul[@class='c-infoBox']/li[h4/text()='حداقل مدرک تحصیلی']/div/span/text()").getall()
@@ -158,14 +166,43 @@ class JobinjaSpider(CrawlSpider):
                 [" ".join([word.strip() for word in ms.split() if word != ""])
                  for ms in militaryServiceStatus if ms != ""])
 
-        f = codecs.open(self.company_jobs_details, "a", encoding="utf-8")
-        row_csv = "Company :{0},title:{1},categories: {2},location: {3}, jobType: {4},minExperience:{5},salary: {6}," \
-                  "skills:{7}, gender: {8}, allowedMajors:{9},active: {10},period: {11}, degree: {12}," \
-                  "language: {13},desc: {14}, company_desc: {15},link:{16}\r\n"\
-            .format(item["company_fa"], item["name"], item["category"],
-                    item["location"], item["jobType"], item["minExperience"],
-                    item["salary"], item["skills"], item["gender"], item["allowedMajors"],
-                    item["active"], item["period"], item["degree"], item["language"],
-                    item["desc"], item["company_desc"], response.request.url)
+        self.write_job_info(item)
+        yield item
+
+    def write_company_info(self, item, header=False):
+
+        if not header:
+            f = codecs.open(self.company_details, "a", encoding="utf-8")
+            row_csv = "{0},{1},{2},{3},{4},{5},{6}\r\n".format(item["title_fa"], item["title_en"], item["open_jobs"],
+                                                           item["category"], item["company_size"], item["company_site"],
+                                                           item["year"])
+        else:
+            f = codecs.open(self.company_details, "w", encoding="utf-8")
+            row_csv = "{0},{1},{2},{3},{4},{5},{6}\r\n".format("title_fa", "title_en", "open_jobs",
+                                                               "category", "company_size", "company_site",
+                                                               "year")
+        f.write(row_csv)
+        f.close()
+
+    def write_job_info(self, item, header=False):
+        if not header:
+            f = codecs.open(self.company_jobs_details, "a", encoding="utf-8")
+            row_csv = "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12}," \
+                      "{13},{14},{15}\r\n" \
+                .format(item["company_fa"], item["name"], item["category"],
+                        item["location"], item["jobType"], item["minExperience"],
+                        item["salary"], item["skills"], item["gender"], item["allowedMajors"],
+                        item["active"], item["period"], item["degree"], item["language"],
+                        item["desc"], item["company_desc"])
+        else:
+            f = codecs.open(self.company_jobs_details, "w", encoding="utf-8")
+            row_csv = "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12}," \
+                      "{13},{14},{15}\r\n" \
+                .format("company_fa", "name", "category",
+                        "location", "jobType", "minExperience",
+                        "salary", "skills", "gender", "allowedMajors",
+                        "active", "period", "degree", "language",
+                        "desc", "company_desc")
+
         f.write(row_csv)
         f.close()
