556c3743dac8d0872f84a3e4c3e24a65f066ca75
mojta <mojtaba.banaie@gmail.com>
2019-03-24 15:12:46 +0430

    adding persian comment for item classes

--- jobinja_crawler/jobinja_crawler/items.py
+++ jobinja_crawler/jobinja_crawler/items.py
@@ -9,30 +9,31 @@ import scrapy
 
 
 class JobinjaCompanyItem(scrapy.Item):
-    title_fa = scrapy.Field()
-    title_en = scrapy.Field()
-    open_jobs= scrapy.Field()
-    category = scrapy.Field()
-    company_size = scrapy.Field()
-    company_site = scrapy.Field()
-    year = scrapy.Field()
+    title_fa = scrapy.Field()  # عنوان فارسی شرکت
+    title_en = scrapy.Field()  # عنوان لاتین شرکت
+    open_jobs = scrapy.Field()  # تعداد شغل‌های باز و موردنیاز
+    category = scrapy.Field()  # گروه شغلی
+    company_size = scrapy.Field()  # اندازه شرکت
+    company_site = scrapy.Field()  # سایت شرکت
+    year = scrapy.Field()  # سال تاسیس
+
 
 class JobinjaJobItem(scrapy.Item):
     # define the fields for your item here like:
-    name = scrapy.Field()
-    company_fa = scrapy.Field()
-    category = scrapy.Field()
-    location = scrapy.Field()
-    minExperience = scrapy.Field()
-    jobType = scrapy.Field()
-    salary = scrapy.Field()
-    desc = scrapy.Field()
-    company_desc = scrapy.Field()
-    skills = scrapy.Field()
-    period = scrapy.Field()
-    militaryServiceStatus = scrapy.Field()
-    gender = scrapy.Field()
-    degree = scrapy.Field()
-    language = scrapy.Field()
-    allowedMajors = scrapy.Field()
-    active = scrapy.Field()
+    name = scrapy.Field()  # عنوان آگهی
+    company_fa = scrapy.Field()  # نام شرکت به فارسی
+    category = scrapy.Field()  # گروه
+    location = scrapy.Field()  # مکان
+    minExperience = scrapy.Field()  # حداقل سابقه کار مورد نیاز
+    jobType = scrapy.Field()  # نوع شغلی
+    salary = scrapy.Field()  # حقوق
+    desc = scrapy.Field()  # توضیح
+    company_desc = scrapy.Field()  # توضیح شرکت
+    skills = scrapy.Field()  # مهارت های مورد نیاز
+    period = scrapy.Field()  # بازه زمانی اشتغال
+    militaryServiceStatus = scrapy.Field()  # وضعیت نظام وظیفه
+    gender = scrapy.Field()  # جنسیت
+    degree = scrapy.Field()  # حداقل مدرک مورد نیاز
+    language = scrapy.Field()  # زبان‌های مورد نیاز
+    allowedMajors = scrapy.Field()  # مدارک تحصیلی مورد نیاز
+    active = scrapy.Field()  # فعال یا غیر فعال بودن آگهی
